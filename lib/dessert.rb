bcad4039938ca41ec8f3ba2d613c8d4aedeb57f6
Josh <jdvorak@citadel.edu>
2016-09-20 17:47:21 +0000

    dessert class modified

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,33 @@
 class Dessert
+  
+  attr_accessor :name, :calories
+  
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
+  
   def healthy?
     # your code here
   end
+  
   def delicious?
     # your code here
   end
 end
 
 class JellyBean < Dessert
+  
+  attr_accessor :flavor
+  
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    @calories = 5
+    @name = flavor + ' jelly bean'
   end
+  
+  def delicious?
+    #return#
+    @flavor != 'licorice'
+  end  
 end
